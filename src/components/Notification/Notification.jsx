b32f738b3827f033da2e6474a705cad9f66dde70
Julia Rudenko <yrudenko@i.ua>
2023-01-13 18:22:08 +0200

    Notification

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
+import {Message } from './Notification.styled';
 
 const Notification = ({ message = '' }) => (
   <>
-    <p>{message}</p>
+    <Message>{message}</Message>
   </>
 );
 
